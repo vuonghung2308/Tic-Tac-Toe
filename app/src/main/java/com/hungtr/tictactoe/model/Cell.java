4ead826f2274e91be24bd658409896588f50c4c3
Hung <tr2hung99@gmail.com>
2021-06-06 16:30:28 +0700

    vbbn

---
+++ app/src/main/java/com/hungtr/tictactoe/model/Cell.java
@@ -0,0 +1,16 @@
+package com.hungtr.tictactoe.model;
+
+import com.hungtr.tictactoe.model.utilities.StringUtility;
+
+public class Cell {
+
+    public Player player;
+
+    public Cell(Player player) {
+        this.player = player;
+    }
+
+    public boolean isEmpty() {
+        return player == null || StringUtility.isNullOrEmpty(player.value);
+    }
+}
